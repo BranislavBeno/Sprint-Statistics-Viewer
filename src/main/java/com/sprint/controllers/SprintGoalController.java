6ffdac3fe2b79ef476169698253b822789a5acfe
Branislav Beno <brano.beno@gmail.com>
2020-04-17 23:30:58 +0200

    Finishad implementation for reading goals from database

--- src/main/java/com/sprint/controllers/SprintGoalController.java
+++ src/main/java/com/sprint/controllers/SprintGoalController.java
@@ -9,10 +9,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sprint.model.SprintGoal;
 import com.sprint.repository.SprintGoalDAO;
 
@@ -22,6 +26,8 @@ import com.sprint.repository.SprintGoalDAO;
  */
 @Controller
 public class SprintGoalController {
+
+	private static Log log = LogFactory.getLog(SprintGoalController.class);
 
 	private SprintGoalDAO sprintGoals;
 
@@ -37,12 +43,37 @@ public class SprintGoalController {
 		// Sort list of database tables
 		Collections.sort(teams, (a, b) -> a.getTeamName().compareTo(b.getTeamName()));
 
-		SprintGoal teamOne = Optional.ofNullable(teams.get(0)).orElse(new SprintGoal());
-
-		model.addAttribute("sprintLabel", teamOne.getSprintLabel());
-		model.addAttribute("teamOneName", teamOne.getTeamName());
-		// model.addAttribute("teamOneGoals", teamOne.getSprintGoals());
+		// Prepare data for all teams
+		prepareTeamData(model, teams);
 
 		return "goals";
+	}
+
+	private void prepareTeamData(Model model, List<SprintGoal> teams) {
+		for (int i = 0; i < 4; i++) {
+			// Extract team
+			SprintGoal team = Optional.ofNullable(teams.get(i)).orElse(new SprintGoal());
+
+			// Initialize array for goals
+			String[] goals = new String[] {};
+
+			// Initialize jackson mapper for json string
+			ObjectMapper mapper = new ObjectMapper();
+			// Fill array goals
+			try {
+				goals = mapper.readValue(team.getSprintGoals(), String[].class);
+			} catch (JsonProcessingException e) {
+				log.info("Conversion from json to list of goals for team " + team.getTeamName() + " failed.");
+			}
+
+			// Prepare attribute names
+			String teamName = "teamName" + i;
+			String teamGoals = "teamGoals" + i;
+
+			// Add model attributes for thymeleaf template
+			model.addAttribute("sprintLabel", team.getSprintLabel());
+			model.addAttribute(teamName, team.getTeamName());
+			model.addAttribute(teamGoals, goals);
+		}
 	}
 }
