8194f5592dd9b124ed1c80fe17e8c23d8545ff98
Branislav Beno <brano.beno@gmail.com>
2020-05-03 17:07:34 +0200

    Slimmer active side bar - final solution

--- src/main/resources/static/js/Sidebar.js
+++ src/main/resources/static/js/Sidebar.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 	$('#sidebarCollapse').on('click', function() {
+		// Switch active state
 		$('#sidebar').toggleClass('active');
+		// If side bar in active state, change element inner html
+		if ($("#sidebar").hasClass("active")) {
+			// Menu item - Home
+			$("#homeMenuItem").html("<i class=\"fas fa-home\"></i>");
+			// Menu item - Progress
+			$("#progressMenuItem").html("<i class=\"far fa-chart-bar\"></i>");
+			// Menu item - KPI's
+			$("#kpiMenuItem").html("<i class=\"fas fa-th\"></i>");
+			// Menu item - Goals
+			$("#goalsMenuItem").html("<i class=\"fas fa-list-ul\"></i>");
+		} else {
+			// Menu item - Home
+			$("#homeMenuItem").html("<i class=\"fas fa-home\"></i> Home");
+			// Menu item - Progress
+			$("#progressMenuItem").html("<i class=\"far fa-chart-bar\"></i> Progress");
+			// Menu item - KPI's
+			$("#kpiMenuItem").html("<i class=\"fas fa-th\"></i> KPI's");
+			// Menu item - Goals
+			$("#goalsMenuItem").html("<i class=\"fas fa-list-ul\"></i> Goals");
+		}
 	});
 });
