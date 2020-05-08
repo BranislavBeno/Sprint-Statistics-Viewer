dc63ab10a4ee7c433b8cab8960bf664486c6eac2
Branislav Beno <brano.beno@gmail.com>
2020-05-08 16:13:51 +0200

    DataSource configured programmatically

--- src/main/java/com/sprint/config/SprintConfig.java
+++ src/main/java/com/sprint/config/SprintConfig.java
@@ -5,9 +5,12 @@ package com.sprint.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
@@ -17,6 +20,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
  */
 @Configuration
 @ComponentScan("com.sprint")
+@PropertySource("classpath:persistence-mysqldb.properties")
 public class SprintConfig {
 
 	/**
@@ -25,12 +29,12 @@ public class SprintConfig {
 	 * @return the data source
 	 */
 	@Bean
-	public DataSource mysqlDataSource() {
+	public DataSource mysqlDataSource(@Autowired Environment env) {
 		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/sprints");
-		dataSource.setUsername("sprints");
-		dataSource.setPassword("sprints");
+        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getProperty("jdbc.url"));
+        dataSource.setUsername(env.getProperty("jdbc.user"));
+        dataSource.setPassword(env.getProperty("jdbc.pass"));
 
 		return dataSource;
 	}
