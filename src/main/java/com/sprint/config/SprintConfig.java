08d54af7fb5df1a367c83d3b8ed691ab73fcb1f4
Branislav Beno <brano.beno@gmail.com>
2020-05-15 18:25:26 +0200

    docker-compose.yml - JDBC connection parameters are now rewritable through docker-compose configuration

--- src/main/java/com/sprint/config/SprintConfig.java
+++ src/main/java/com/sprint/config/SprintConfig.java
@@ -8,7 +8,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -18,7 +17,6 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
  * @author benito
  */
 @Configuration
-@PropertySource("classpath:persistence-mysqldb.properties")
 public class SprintConfig {
 
 	/**
@@ -30,10 +28,10 @@ public class SprintConfig {
 	public DataSource mysqlDataSource(@Autowired Environment env) {
 		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-		dataSource.setUrl(env.getProperty("jdbc.url"));
-		dataSource.setUsername(env.getProperty("jdbc.user"));
-		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(env.getProperty("spring.datasource.url"));
+		dataSource.setUsername(env.getProperty("spring.datasource.username"));
+		dataSource.setPassword(env.getProperty("spring.datasource.password"));
 
 		return dataSource;
 	}
