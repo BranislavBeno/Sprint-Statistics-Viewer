a600e75e85458263d2dbe20f9b9a989ac8dac9a4
Branislav Beno <brano.beno@gmail.com>
2020-06-09 00:13:21 +0200

    Started work on about page

--- src/main/java/com/sprint/controllers/AboutController.java
+++ src/main/java/com/sprint/controllers/AboutController.java
@@ -3,13 +3,17 @@ package com.sprint.controllers;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class IndexController implements ErrorController {
+public class AboutController implements ErrorController {
 
 	@Override
-    @GetMapping("/about")
+    @GetMapping("/error")
+    @ResponseBody
 	public String getErrorPath() {
-        return "about";
+		// Error occurred
+        return "Error occurred";
 	}
+
 }
