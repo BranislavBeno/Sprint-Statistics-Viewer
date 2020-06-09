301b3ab6eeca66c0bfc3bac16010aea022014fb5
Branislav Beno <brano.beno@gmail.com>
2020-06-10 00:26:06 +0200

    Work on about page in progress

--- src/main/java/com/sprint/controllers/AboutController.java
+++ src/main/java/com/sprint/controllers/AboutController.java
@@ -1,19 +1,13 @@
 package com.sprint.controllers;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class AboutController implements ErrorController {
+public class AboutController {
 
-	@Override
-    @GetMapping("/error")
-    @ResponseBody
-	public String getErrorPath() {
-		// Error occurred
-        return "Error occurred";
+	@GetMapping("/about")
+	public String about() {
+		return "about";
 	}
-
 }
--- src/main/java/com/sprint/controllers/IndexController.java
+++ src/main/java/com/sprint/controllers/IndexController.java
@@ -3,13 +3,16 @@ package com.sprint.controllers;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController implements ErrorController {
 
 	@Override
-    @GetMapping("/about")
+    @GetMapping("/error")
+    @ResponseBody
 	public String getErrorPath() {
-        return "about";
+		// Error occurred
+        return "Error occurred";
 	}
 }
