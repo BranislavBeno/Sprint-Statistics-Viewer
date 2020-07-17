19acda50fd3d2b5691cac6b94ec251e502dc795f
Branislav Beno <brano.beno@gmail.com>
2020-07-17 23:11:33 +0200

    Added integration tests for SprintKpiController class

--- src/test/java/com/sprint/controllers/SprintKpiControllerTest.java
+++ src/test/java/com/sprint/controllers/SprintKpiControllerTest.java
@@ -10,21 +10,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.ext.ScriptUtils;
+import org.testcontainers.jdbc.JdbcDatabaseDelegate;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.sprint.extension.ScreenCaptureOnFailure;
+import com.sprint.repository.DatabaseBaseTest;
+import com.sprint.repository.impl.SprintKpiDAO;
 
-/**
- * The Class AboutControllerTest.
- */
 @Testcontainers
 @ExtendWith({ ScreenCaptureOnFailure.class })
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AboutControllerTest {
+class SprintKpiControllerTest extends DatabaseBaseTest {
+
+	@Autowired
+	private SprintKpiDAO kpis;
 
 	/** The container. */
 	@Container
@@ -42,30 +47,29 @@ class AboutControllerTest {
 	 */
 	@BeforeEach
 	private void getWebDriverUrl() {
-		container.getWebDriver().get("http://172.17.0.1:" + port + "/about");
+		// Load data from data source
+		ScriptUtils.runInitScript(new JdbcDatabaseDelegate(DATABASE, ""), "CREATE_AND_INITIALIZE_TEAM_TABLE.sql");
+		kpis.setDataSource(dataSource());
+
+		// Get web driver's URL
+		container.getWebDriver().get("http://172.17.0.1:" + port + "/kpi?sprint=");
 	}
 
-	/**
-	 * Test about page title.
-	 */
 	@Test
-	@DisplayName("Test whether page title is 'About'")
+	@DisplayName("Test whether page title is 'Sprint KPI's Sprint 2'")
 	void testAboutPageTitle() {
 		// Get page title
 		WebElement pageTitle = container.getWebDriver().findElementByTagName("title");
 		// Assert expected and actual content
-		assertThat(pageTitle.getAttribute("text")).isEqualTo("About");
+		assertThat(pageTitle.getAttribute("text")).isEqualTo("Sprint KPI's Sprint 2");
 	}
 
-	/**
-	 * Test unsorted list of web elements.
-	 */
 	@Test
-	@DisplayName("Test whether unordered web elements list has size 8")
-	void testUnsortedWebElementsList() {
+	@DisplayName("Test whether third table column has title 'Team Mango'")
+	void testHeaderElementsFromTable() {
 		// Get list of web elements
-		List<WebElement> webElementList = container.getWebDriver().findElementsByCssSelector(".lead li");
+		List<WebElement> webElementList = container.getWebDriver().findElementsByCssSelector("thead tr th");
 		// Assert expected and actual content
-		assertThat(webElementList.size()).isEqualTo(8);
+		assertThat(webElementList.get(2).getText()).isEqualTo("Team Mango");
 	}
 }
