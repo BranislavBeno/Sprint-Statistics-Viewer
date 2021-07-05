f55def378cc72383a26552011b34975b7842ff3a
Branislav Beno <brano.beno@gmail.com>
2021-07-05 22:53:46 +0200

    Use selenide for frontend test

--- src/test/java/com/sprint/controllers/AboutControllerTest.java
+++ src/test/java/com/sprint/controllers/AboutControllerTest.java
@@ -1,64 +1,70 @@
 package com.sprint.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.junit5.ScreenShooterExtension;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.WebElement;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import com.sprint.extension.ScreenCaptureOnFailure;
-
-/**
- * The Class AboutControllerTest.
- */
-@Testcontainers
-@ExtendWith({ ScreenCaptureOnFailure.class })
+@Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AboutControllerTest {
 
-	/** The port. */
-	@LocalServerPort
-	private int port;
+  @Container
+  public static final BrowserWebDriverContainer<?> webDriverContainer =
+      new BrowserWebDriverContainer<>()
+          .withCapabilities(new ChromeOptions()
+              .addArguments("--no-sandbox")
+              .addArguments("--disable-dev-shm-usage"));
 
-	/**
-	 * Prepare resources.
-	 */
-	@BeforeEach
-	private void prepareResources() {
-		ChromeBrowserInitializer.WEB_DRIVER_CONTAINER.getWebDriver()
-				.get(ChromeBrowserInitializer.URL + port + "/about");
-	}
+  @RegisterExtension
+  public static ScreenShooterExtension screenShooterExtension =
+      new ScreenShooterExtension().to("target/selenide");
 
-	/**
-	 * Test about page title.
-	 */
-	@Test
-	@DisplayName("Test whether page title is 'About'")
-	void testAboutPageTitle() {
-		// Get page title
-		WebElement pageTitle = ChromeBrowserInitializer.WEB_DRIVER_CONTAINER.getWebDriver()
-				.findElementByCssSelector(".blockquote h3");
-		// Assert expected and actual content
-		assertThat(pageTitle.getText()).isEqualTo("About");
-	}
+  @LocalServerPort
+  private Integer port;
 
-	/**
-	 * Test unsorted list of web elements.
-	 */
-	@Test
-	@DisplayName("Test whether unordered web elements list has size 8")
-	void testUnsortedWebElementsList() {
-		// Get list of web elements
-		List<WebElement> webElementList = ChromeBrowserInitializer.WEB_DRIVER_CONTAINER.getWebDriver()
-				.findElementsByCssSelector(".lead li");
-		// Assert expected and actual content
-		assertThat(webElementList.size()).isEqualTo(8);
-	}
+  @Test
+  @DisplayName("Test whether page title is 'About'")
+  void testAboutPageTitle() {
+    Configuration.timeout = 2000;
+    Configuration.baseUrl = "http://172.17.0.1:" + port;
+
+    RemoteWebDriver remoteWebDriver = webDriverContainer.getWebDriver();
+    WebDriverRunner.setWebDriver(remoteWebDriver);
+
+    open("/about");
+    String caption = $(By.tagName("title")).getOwnText();
+    assertThat(caption).isEqualTo("About");
+  }
+
+  @Test
+  @DisplayName("Test whether unordered web elements list has size 8")
+  void testUnsortedWebElementsList() {
+    Configuration.timeout = 2000;
+    Configuration.baseUrl = "http://172.17.0.1:" + port;
+
+    RemoteWebDriver remoteWebDriver = webDriverContainer.getWebDriver();
+    WebDriverRunner.setWebDriver(remoteWebDriver);
+
+    open("/about");
+    List<WebElement> elementList = $(By.className("lead")).findElements(By.cssSelector("li"));
+    assertThat(elementList.size()).isEqualTo(8);
+  }
 }
