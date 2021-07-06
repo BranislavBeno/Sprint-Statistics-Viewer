1a44856eeefdd2446d8c6e87b6fe2e90fa10a676
Branislav Beno <brano.beno@gmail.com>
2021-07-06 23:36:54 +0200

    Rework TeamRefinementControllerTest

--- src/test/java/com/sprint/controllers/TeamRefinementControllerTest.java
+++ src/test/java/com/sprint/controllers/TeamRefinementControllerTest.java
@@ -1,63 +1,81 @@
 package com.sprint.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.testcontainers.ext.ScriptUtils;
-import org.testcontainers.jdbc.JdbcDatabaseDelegate;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import com.sprint.extension.ScreenCaptureOnFailure;
 import com.sprint.repository.DatabaseBaseTest;
 import com.sprint.repository.impl.TeamRefinementDAO;
 import com.sprint.repository.impl.TeamVelocityDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.ext.ScriptUtils;
+import org.testcontainers.jdbc.JdbcDatabaseDelegate;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
-@ExtendWith({ ScreenCaptureOnFailure.class })
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith({ScreenCaptureOnFailure.class})
+@Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TeamRefinementControllerTest extends DatabaseBaseTest {
 
-	@Autowired
-	private TeamRefinementDAO refinements;
+  @Autowired
+  private TeamRefinementDAO refinements;
 
-	@Autowired
-	private TeamVelocityDAO velocity;
+  @Autowired
+  private TeamVelocityDAO velocity;
 
-	/** The port. */
-	@LocalServerPort
-	private int port;
+  @Container
+  public static final BrowserWebDriverContainer<?> webDriverContainer =
+      new BrowserWebDriverContainer<>()
+          .withCapabilities(new ChromeOptions()
+              .addArguments("--no-sandbox")
+              .addArguments("--disable-dev-shm-usage"));
 
-	/**
-	 * Prepare resources.
-	 */
-	@BeforeEach
-	private void prepareResources() {
-		// Load data from data source
-		ScriptUtils.runInitScript(new JdbcDatabaseDelegate(DATABASE, ""), "CREATE_AND_INITIALIZE_TEAM_TABLE.sql");
+  @RegisterExtension
+  public static ScreenShooterExtension screenShooterExtension =
+      new ScreenShooterExtension().to("build/selenide");
 
-		// Read refinement specific data
-		refinements.setDataSource(dataSource());
+  @LocalServerPort
+  private int port;
 
-		// Read velocity specific data
-		velocity.setDataSource(dataSource());
+  @BeforeEach
+  void setUp() {
+    // Load data from data source
+    ScriptUtils.runInitScript(new JdbcDatabaseDelegate(DATABASE, ""), "CREATE_AND_INITIALIZE_TEAM_TABLE.sql");
 
-		// Get web driver's URL
-		ChromeBrowserInitializer.WEB_DRIVER_CONTAINER.getWebDriver()
-				.get(ChromeBrowserInitializer.URL + port + "/apple/refinement");
-	}
+    // Read refinement specific data
+    refinements.setDataSource(dataSource());
+    // Read velocity specific data
+    velocity.setDataSource(dataSource());
 
-	@Test
-	@DisplayName("Test whether page title is 'Team Apple refinement'")
-	void testAboutPageTitle() {
-		// Get page title
-		String pageTitle = ChromeBrowserInitializer.WEB_DRIVER_CONTAINER.getWebDriver().getTitle();
-		// Assert expected and actual content
-		assertThat(pageTitle).isEqualTo("Team Apple refinement");
-	}
+    Configuration.timeout = 2000;
+    Configuration.baseUrl = "http://172.17.0.1:" + port;
+
+    RemoteWebDriver remoteWebDriver = webDriverContainer.getWebDriver();
+    WebDriverRunner.setWebDriver(remoteWebDriver);
+
+    open("/apple/refinement");
+  }
+
+  @Test
+  @DisplayName("Test whether page title is 'Team Apple refinement'")
+  void testPageTitle() {
+    String caption = $(By.tagName("title")).getOwnText();
+    assertThat(caption).isEqualTo("Team Apple refinement");
+  }
 }
