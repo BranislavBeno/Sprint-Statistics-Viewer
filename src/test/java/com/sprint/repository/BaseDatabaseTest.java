f868e314761a12ac9543a3e4840cdd535cb75d42
Branislav Beno <brano.beno@gmail.com>
2022-08-30 22:09:25 +0200

    Upgrade to java 18

--- src/test/java/com/sprint/repository/BaseDatabaseTest.java
+++ src/test/java/com/sprint/repository/BaseDatabaseTest.java
@@ -9,7 +9,7 @@ abstract class BaseDatabaseTest {
     public static final MySQLContainer<?> DATABASE = populateDatabase();
 
     private static MySQLContainer<?> populateDatabase() {
-        try (MySQLContainer<?> container = new MySQLContainer<>("mysql:8.0.28")) {
+        try (MySQLContainer<?> container = new MySQLContainer<>("mysql:8.0.30")) {
             container.withReuse(true);
 
             return container;
